179c73c1d2951a1b3d12bdf083242361e4a4c4b7
Pepe Valdivia <jvaldivia@softweb.pe>
2018-02-08 20:36:55 -0500

    Hook para todos los requests

--- app.pl
+++ app.pl
@@ -1,17 +1,17 @@
 use Mojolicious::Lite;
 
-get '/' => {
-  text => 'I ♥ Mojolicious!'
-};
-
 app->hook(after_dispatch => sub {
   my $c = shift;
   my $headers = $c->req->headers;
   my $origin = $headers->origin;
-  say "Origin: $origin";
+  #say "Origin: $origin";
   $c->res->headers->header('Server' => 'Ubuntu');
   $c->res->headers->header('x-powered-by' => 'Mojolicious (Perl)');
-  $c->res->headers->header('Access-Control-Allow-Origin' => $origin);
+  #$c->res->headers->header('Access-Control-Allow-Origin' => $origin);
 });
+
+get '/' => {
+  text => 'I ♥ Mojolicious!'
+};
 
 app->start;
